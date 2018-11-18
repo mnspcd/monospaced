1bdb08c9e7a885cbc938fb00f3f71f84baf0309f
monospaced <scott@monospaced.com>
2018-11-18 20:56:30 +0000

    fix: update lighthouse json query syntax
    Closes #7

--- bin/deploy-lighthouse.sh
+++ bin/deploy-lighthouse.sh
@@ -3,20 +3,18 @@
 JSON="build/lighthouse.report.json"
 TMP=$(mktemp)
 
-SCORE=$(cat $JSON | jq ".score")
-SCORE_0=$(cat $JSON | jq ".reportCategories[0] .score")
-SCORE_1=$(cat $JSON | jq ".reportCategories[1] .score")
-SCORE_2=$(cat $JSON | jq ".reportCategories[2] .score")
-SCORE_3=$(cat $JSON | jq ".reportCategories[3] .score")
-SCORE_4=$(cat $JSON | jq ".reportCategories[4] .score")
+SCORE_0=$(cat $JSON | jq ".categories .performance .score")
+SCORE_1=$(cat $JSON | jq ".categories .pwa .score")
+SCORE_2=$(cat $JSON | jq ".categories .accessibility .score")
+SCORE_3=$(cat $JSON | jq '.categories ."best-practices" .score')
+SCORE_4=$(cat $JSON | jq ".categories .seo .score")
 
 {
-jq ".score = $(printf "%.0f\n" $SCORE)" | \
-jq ".reportCategories[0] .score = $(printf "%.0f\n" $SCORE_0)" | \
-jq ".reportCategories[1] .score = $(printf "%.0f\n" $SCORE_1)" | \
-jq ".reportCategories[2] .score = $(printf "%.0f\n" $SCORE_2)" | \
-jq ".reportCategories[3] .score = $(printf "%.0f\n" $SCORE_3)" | \
-jq ".reportCategories[4] .score = $(printf "%.0f\n" $SCORE_4)"
+jq ".reportCategories[0] .score = $(printf $SCORE_0*100)" | \
+jq ".reportCategories[1] .score = $(printf $SCORE_1*100)" | \
+jq ".reportCategories[2] .score = $(printf $SCORE_2*100)" | \
+jq ".reportCategories[3] .score = $(printf $SCORE_3*100)" | \
+jq ".reportCategories[4] .score = $(printf $SCORE_4*100)"
 } < $JSON > "$TMP" && mv "$TMP" $JSON
 
 . ./bin/deploy.sh lighthouse.report.html
