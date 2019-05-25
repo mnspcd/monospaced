fc0d86dee4739448ff37b0faf92023cadaaa2408
monospaced <scott@monospaced.com>
2019-05-25 22:37:42 +0100

    build: convert .babelrc to babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = api => {
+  api.cache(true);
+  return {
+    presets: ["@babel/preset-env", "@babel/preset-react"],
+    plugins: [
+      "@babel/plugin-proposal-object-rest-spread",
+      [
+        "inline-react-svg",
+        {
+          svgo: false,
+        },
+      ],
+    ],
+  };
+};
