d4a5a60769fcdbad76e2b944a0316a56e1c10095
monospaced <scott@monospaced.com>
2019-11-16 23:55:20 +0000

    feat: rework app entry

--- src/index.js
+++ src/index.js
@@ -1,14 +1,106 @@
 import "core-js/es/array/includes";
 import "core-js/es/object/assign";
 import "core-js/es/string/includes";
+import PropTypes from "prop-types";
 import React from "react";
 import DocumentMeta from "react-document-meta";
 import ReactDOM from "react-dom";
 import ReactDOMServer from "react-dom/server";
 import { browserHistory, createMemoryHistory, Router } from "react-router";
 
-import Root from "./components/Root";
+import { version } from "../package.json";
+
+import tokens from "./@monospaced/modern/tokens";
+import "./assets/.htaccess";
+import "./assets/android-chrome-192x192.png";
+import "./assets/android-chrome-512x512.png";
+import "./assets/apple-touch-icon.png";
+import "./assets/browserconfig.xml";
+import "./assets/favicon.ico";
+import "./assets/favicon-16x16.png";
+import "./assets/favicon-32x32.png";
+import "./assets/manifest.webmanifest";
+import "./assets/mstile-150x150.png";
+import "./assets/safari-pinned-tab.svg";
 import routes from "./routes";
+
+const Document = ({ meta, router }) => {
+  const themeColor = tokens["color-brand-primary"];
+
+  return (
+    <html lang="en-gb">
+      <head>
+        <meta charSet="utf-8" />
+        {meta}
+        <meta content={themeColor} name="msapplication-TileColor" />
+        <meta content={themeColor} name="theme-color" />
+        <meta content="width=device-width" name="viewport" />
+        <link
+          href="/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/manifest.webmanifest" rel="manifest" />
+        <link
+          color={themeColor}
+          href="/safari-pinned-tab.svg"
+          rel="mask-icon"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/assets/37BCE3_2_0.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/assets/37BCE3_3_0.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link
+          as="font"
+          crossOrigin="anonymous"
+          href="/assets/37BCE3_5_0.woff2"
+          rel="preload"
+          type="font/woff2"
+        />
+        <link href={`/styles.css?v=${version}`} rel="stylesheet" />
+      </head>
+      <body className="no-js" style={{ margin: "0" }}>
+        <script
+          dangerouslySetInnerHTML={{ __html: `document.body.className = ""` }}
+        />
+        <div dangerouslySetInnerHTML={router} id="router" />
+        <script src={`/bundle.js?v=${version}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `var c = document.createElement("link");c.rel = "stylesheet";c.href = "//hello.myfonts.net/count/37bce3";var h = document.head;n = h.childNodes;h.insertBefore(c, n[n.length - 1 ].nextSibling);`,
+          }}
+        />
+      </body>
+    </html>
+  );
+};
+
+Document.propTypes = {
+  meta: PropTypes.array.isRequired,
+  router: PropTypes.array.isRequired,
+};
 
 if (typeof document !== "undefined") {
   const router = document.getElementById("router");
@@ -34,7 +126,7 @@ export default ({ path }, callback) => {
   };
   const meta = DocumentMeta.renderAsReact();
   const html = ReactDOMServer.renderToStaticMarkup(
-    <Root meta={meta} router={router} />,
+    <Document meta={meta} router={router} />,
   );
 
   callback(null, `<!DOCTYPE html>${html}`);
