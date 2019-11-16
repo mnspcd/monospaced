463e28fada4e09beaa219e7e4ea581079b721db1
monospaced <scott@monospaced.com>
2019-11-16 23:48:26 +0000

    build: update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,19 @@
-const { extensions, variables } = require("./src/system");
+const {
+  color,
+  media,
+  opacity,
+  typography,
+} = require("./src/@monospaced/modern/tokens");
 
 module.exports = () => ({
   plugins: {
     "postcss-import": {},
     "postcss-cssnext": {
       features: {
-        customMedia: { extensions },
-        customProperties: { variables },
+        customMedia: { extensions: media },
+        customProperties: {
+          variables: Object.assign({}, color, opacity, typography),
+        },
       },
     },
   },
